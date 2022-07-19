f9bb37656e0f7e7483546c57f981fea07b72d304
Aryan Sharma <97798738+Aryan5s@users.noreply.github.com>
2022-07-19 16:06:58 +0530

    Time: 1 ms (53.06%), Space: 40.9 MB (55.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,22 +4,32 @@ class Solution {
         int dp[][]=new int[m][n];
     for (int[] row : dp)
         Arrays.fill(row, -1);
-    return countWaysUtil(m-1,n-1,dp);
+    return countWaysUtil(m,n,dp);
         
     }
        
-    public int countWaysUtil(int i, int j, int[][] dp) {
-  if(i==0 && j == 0)
-    return 1;
-  if(i<0 || j<0)
-    return 0;
-  if(dp[i][j]!=-1) return dp[i][j];
-    
-  int up = countWaysUtil(i-1,j,dp);
-  int left = countWaysUtil(i,j-1,dp);
+    public int countWaysUtil(int m, int n, int[][] dp) {
+    for(int i=0; i<m ;i++){
+      for(int j=0; j<n; j++){
+          
+          //base condition
+          if(i==0 && j==0){
+              dp[i][j]=1;
+              continue;
+          }
+          
+          int up=0;
+          int left = 0;
+          
+          if(i>0) 
+            up = dp[i-1][j];
+          if(j>0)
+            left = dp[i][j-1];
+            
+          dp[i][j] = up+left;
+      }
+  }
   
-  return dp[i][j] = up+left;
-  
-}
-   
+  return dp[m-1][n-1];
     }
+}
