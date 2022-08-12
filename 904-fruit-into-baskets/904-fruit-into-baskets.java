f0c6b4ee5d427bdbaf93ad19d048fa12e1eb908f
Aryan Sharma <97798738+Aryan5s@users.noreply.github.com>
2022-08-12 15:19:54 +0530

    Time: 19 ms (79.75%), Space: 72.5 MB (74.72%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int last_fruit = -1;
+        int second_last_fruit = -1;
+        int last_fruit_count = 0;
+        int current_max = 0;
+        int max = 0;
+        
+        for(Integer fruit : fruits){
+            
+            if(fruit == last_fruit || fruit == second_last_fruit){
+                current_max +=1;
+            }else{
+                current_max = last_fruit_count + 1;
+            }
+            
+            if(fruit == last_fruit){
+                last_fruit_count +=1;
+            }else{
+                last_fruit_count = 1;
+            }
+            
+            
+            if(fruit != last_fruit){
+                second_last_fruit = last_fruit;
+                last_fruit = fruit;
+            }
+            
+            max = Math.max(current_max , max);
+        }
+        return max;
+    }
+}
