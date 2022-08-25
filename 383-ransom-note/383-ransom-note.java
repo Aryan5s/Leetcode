775345b43d163dacedaa3180992813e4368692b8
Aryan Sharma <97798738+Aryan5s@users.noreply.github.com>
2022-08-25 10:53:33 +0530

    Time: 5 ms (74.47%), Space: 45.4 MB (79.75%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+          int[] count = new int[26];
+        
+        for(int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 97]++;
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++) {
+            if(count[ransomNote.charAt(i) - 97] == 0)
+                return false;
+            count[ransomNote.charAt(i) - 97]--;
+        }
+        
+        return true;
+    }
+}
